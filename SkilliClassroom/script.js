fbe78dfbda43a7bad6b9ab364db77d4fd0747a04
frontendmyskilli <97792423+frontendmyskilli@users.noreply.github.com>
2022-01-25 00:17:50 -0800

    accordion bug fix

--- SkilliClassroom/script.js
+++ SkilliClassroom/script.js
@@ -67,25 +67,3 @@ closeNavBtn.addEventListener(
 );
 
 
-// function for accordion
-let accordion = document.getElementsByClassName("accordion");
-
-
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function () {
-    if (this.childNodes[1].classList.contains("fa-plus")) {
-      this.childNodes[1].classList.remove("fa-plus");
-      this.childNodes[1].classList.add("fa-minus");
-    } else {
-      this.childNodes[1].classList.remove("fa-minus");
-      this.childNodes[1].classList.add("fa-plus");
-    }
-
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
